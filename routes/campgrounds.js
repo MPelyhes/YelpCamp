7cf201a8ab93c5460525f38472243713560908cd
Matt Pelyhes <mattpelyhes@gmail.com>
2021-02-07 09:08:52 -0600

    add isLoggedIn middleware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
+const { campgroundSchema } = require('../schemas.js');
+const { isLoggedIn } = require('../middleware');
+
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
-const { campgroundSchema } = require('../schemas.js');
 
 const validateCampground = (req, res, next) => {
   const { error } = campgroundSchema.validate(req.body);
   if(error){
     const msg = error.details.map(el => el.message).join(',')
-    throw new ExpressError(msg, 400)
+    throw new ExpressError(msg, 400) 
   } else {
     next();
   }
@@ -20,11 +22,11 @@ router.get('/', async (req, res) => {
   res.render('campgrounds/index', { campgrounds })
 })
 
-router.get('/new', (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
   res.render('campgrounds/new')
 })
 
-router.post('/', validateCampground, catchAsync(async (req, res, next) => {
+router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
   const campground = new Campground(req.body.campground);
   await campground.save();
   req.flash('success', 'Successfully made a new campground!')
@@ -40,7 +42,7 @@ router.get('/:id', catchAsync(async (req, res) => {
   res.render('campgrounds/show', { campground })
 }));
 
-router.get('/:id/edit', catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   if(!campground){
     req.flash('error', 'Cannot find that campground!')
