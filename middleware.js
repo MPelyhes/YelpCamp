047fe4d3ee1e0df6b71b39c096bb634f58f5c15b
Matt Pelyhes <mattpelyhes@gmail.com>
2021-02-08 14:37:56 -0600

    add currentuser helper logic

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 const { modelName } = require("./models/user");
 
 module.exports.isLoggedIn = (req, res, next) => {
+  console.log('req.user...', req.user);
   if(!req.isAuthenticated()) {
     req.flash('error', 'You must be signed in');
     return res.redirect('/login');
